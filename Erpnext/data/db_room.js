c3ad199dac9bacea8c9c0285fa2228ae1921d221
a-pichate <pichate.jea@gmail.com>
2026-03-17 22:49:05 +0700

    Delete Erpnext/data/db_room.js2

--- Erpnext/data/db_room.js
+++
@@ -1,6 +0,0 @@
-[
-  { "id": "101", "type": "Suite", "rate": 4500, "inv_status": "Occupied", "hk_status": "Clean", "guest": "Alex Mercer", "folio": "SO-1001" },
-  { "id": "102", "type": "Suite", "rate": 4500, "inv_status": "Available", "hk_status": "Dirty", "guest": null, "folio": null },
-  { "id": "103", "type": "Standard", "rate": 2200, "inv_status": "Available", "hk_status": "Clean", "guest": null, "folio": null },
-  { "id": "104", "type": "Standard", "rate": 2200, "inv_status": "Available", "hk_status": "Clean", "guest": null, "folio": null }
-]
